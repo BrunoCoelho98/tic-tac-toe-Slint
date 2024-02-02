e4d40b54b83be1c623721577e9a134c8dc9986bd
Bruno <br_coelho98@hotmail.com>
2024-02-02 16:24:52 -0300

    Added wasm

---
+++ pkg/tic_tac_toe.d.ts
@@ -0,0 +1,49 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export function main(): void;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly main: () => void;
+  readonly slint_mock_elapsed_time: (a: number) => void;
+  readonly slint_send_mouse_click: (a: number, b: number, c: number, d: number) => void;
+  readonly slint_send_keyboard_char: (a: number, b: number, c: number) => void;
+  readonly send_keyboard_string_sequence: (a: number, b: number) => void;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_export_2: WebAssembly.Table;
+  readonly wasm_bindgen__convert__closures__invoke1_mut__h150181b5472fd1e0: (a: number, b: number, c: number) => void;
+  readonly wasm_bindgen__convert__closures__invoke0_mut__h80b27afed6b952ab: (a: number, b: number) => void;
+  readonly wasm_bindgen__convert__closures__invoke2_mut__hbadb5c8a0ad83e06: (a: number, b: number, c: number, d: number) => void;
+  readonly wasm_bindgen__convert__closures__invoke1__h0049a0fb4184abfb: (a: number, b: number, c: number) => void;
+  readonly wasm_bindgen__convert__closures__invoke1_mut__h1962a3c9e26d7a2c: (a: number, b: number, c: number) => void;
+  readonly wasm_bindgen__convert__closures__invoke0_mut__h65d1535702da02a9: (a: number, b: number) => void;
+  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h33c17c1252c293be: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_exn_store: (a: number) => void;
+  readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {SyncInitInput} module
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ pkg/tic_tac_toe_bg.wasm.d.ts
@@ -0,0 +1,20 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function main(): void;
+export function slint_mock_elapsed_time(a: number): void;
+export function slint_send_mouse_click(a: number, b: number, c: number, d: number): void;
+export function slint_send_keyboard_char(a: number, b: number, c: number): void;
+export function send_keyboard_string_sequence(a: number, b: number): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function wasm_bindgen__convert__closures__invoke1_mut__h150181b5472fd1e0(a: number, b: number, c: number): void;
+export function wasm_bindgen__convert__closures__invoke0_mut__h80b27afed6b952ab(a: number, b: number): void;
+export function wasm_bindgen__convert__closures__invoke2_mut__hbadb5c8a0ad83e06(a: number, b: number, c: number, d: number): void;
+export function wasm_bindgen__convert__closures__invoke1__h0049a0fb4184abfb(a: number, b: number, c: number): void;
+export function wasm_bindgen__convert__closures__invoke1_mut__h1962a3c9e26d7a2c(a: number, b: number, c: number): void;
+export function wasm_bindgen__convert__closures__invoke0_mut__h65d1535702da02a9(a: number, b: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h33c17c1252c293be(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function __wbindgen_start(): void;
